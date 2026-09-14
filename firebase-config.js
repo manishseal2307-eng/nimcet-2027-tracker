6f9cf9f0e9b5cdbed4b6d2111de42d886b40b8d7
manishseal2307-eng <manishseal2307@gmail.com>
2026-09-14 23:00:10 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-// Paste the Web app configuration from your Firebase project here.
-// Do NOT paste a server/private key here. Web API keys are intended for client apps.
 window.NIMCET_FIREBASE_CONFIG = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyCWdIcs60ukKAWERt0-f8ZCIW0mH7mmUBM",
+  authDomain: "nimcet-2027-tracker.firebaseapp.com",
+  projectId: "nimcet-2027-tracker",
+  storageBucket: "nimcet-2027-tracker.firebasestorage.app",
+  messagingSenderId: "388569999294",
+  appId: "1:388569999294:web:5b10ee169b0a0e5c6afbef"
 };
